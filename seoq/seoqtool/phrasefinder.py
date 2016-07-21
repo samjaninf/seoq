475c65c0cfaf691cf33ed3ec8ab56c649d933c93
shenjessiej <tealsticky.note@gmail.com>
2016-07-21 14:43:07 -0400

    phrase finder
    returns 10 most common 2-word and 3-word phrases

---
+++ seoq/seoqtool/phrasefinder.py
@@ -0,0 +1,30 @@
+import nltk
+import string
+from nltk.collocations import *
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+trigram_measures = nltk.collocations.TrigramAssocMeasures()
+
+
+def get_most_common_phrases(txtfile):
+    # change this to read in your data
+    with open(txtfile) as wordfile:
+        text = wordfile.read()
+
+    exclude = set(string.punctuation)
+    text = ''.join(ch for ch in text if ch not in exclude)
+
+    tokens = nltk.wordpunct_tokenize(text)
+    finderbigram = BigramCollocationFinder.from_words(tokens)
+    findertrigram = TrigramCollocationFinder.from_words(tokens)
+
+    # only phrases that appear 3+ times
+    finderbigram.apply_freq_filter(3)
+    findertrigram.apply_freq_filter(3)
+
+    # return the 10 n-grams with the highest score
+    twowordphrases = finderbigram.nbest(bigram_measures.pmi, 10)
+    threewordphrases = findertrigram.nbest(bigram_measures.pmi, 10)
+
+    return twowordphrases, threewordphrases
+
+print get_most_common_phrases('neopets.txt')
