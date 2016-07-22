fa887199d3ef0d03f1a66c622735cf75f58f0b75
Joyce Duan <polarbear814@gmail.com>
2016-07-22 10:28:28 -0400

    Transferred to new repo

--- seoq/seoqtool/local_listing.py
+++ seoq/seoqtool/local_listing.py
@@ -1,7 +1,9 @@
 import urllib
 import json
+import string
 from qscraper_utils import JSONPrint
 from django.conf import settings
+
 
 class LocalListing(object):
 
@@ -16,10 +18,14 @@ class LocalListing(object):
         if url.find('www.') != -1:
             url = url[url.find('.') + 1:]
 
-        query = url[:url.find('.com')]
-        key = 'AIzaSyCM6HT_sT9W7NHB-riLqxtYllvUU94Ys1k'
+        query = url[:url.find('.')].lower()
+        exclude = set(string.punctuation)
+        query = ''.join(ch for ch in query if ch not in exclude)
 
-        MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
+        key = settings.GOOGLE_PLACES_API_KEY
+        apiURL = str(settings.GOOGLE_PLACES_URL)
+
+        MyUrl = (apiURL +
                  '?query=%s'
                  '&key=%s') % (query, key)
 
@@ -28,14 +34,37 @@ class LocalListing(object):
         jsonRaw = response.read()
         jsonData = json.loads(jsonRaw)
 
-        results = JSONPrint()
-        JSONObject = (results.makeRequest(url, ["red"], 0, "72.194.193.110"))['extra_data']
-        title = (str(JSONObject['page_titles'][0]))
-        print title 
+        #results = JSONPrint()
+        #JSONObject = results.makeRequest(url, ["red"], 0, "72.194.193.110")['extra_data']
 
-        for i in range(0, len(jsonData['results'])):
+        #title = (str(JSONObject['page_titles'][0])).lower().replace(' ', '')
+        #title = ''.join(ch for ch in title if ch not in exclude)
+
+        for i in range(len(jsonData['results'])):
+            print jsonData['results'][i]['name']
+
+        for i in range(len(jsonData['results'])):
+            print(len(jsonData['results'])) 
+            print query[0]
+            print jsonData['results'][i]['name']
+            if query[0] in jsonData['results'][i]['name'].lower():
+                return True
+
+        return False
+
+        '''if len(jsonData['results']) > 0:
+            return True
+        else:
+            return False'''
+
+        '''for i in range(len(jsonData['results'])):
             name = jsonData['results'][i]['name'].lower().replace(' ', '')
+            exclude = set(string.punctuation)
+            name = ''.join(ch for ch in name if ch not in exclude)
+            print('n' + name)
             if query in name:
-                return True    
+                return True
+            if title in name:
+                return True'''
 
         return False
