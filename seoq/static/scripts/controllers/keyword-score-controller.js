869f77c3a445ab45cf8e3f9dc11743b49f7871a2
jariza <l.jlac001@gmail.com>
2016-07-27 15:26:53 -0400

    working on async request

--- seoq/static/scripts/controllers/keyword-score-controller.js
+++ seoq/static/scripts/controllers/keyword-score-controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('seoq').controller('keywordScoreController',	['$scope', function ($scope, Validation) {
+angular.module('seoq').controller('keywordScoreController',	['$scope', '$http', function ($scope, $http) {
 	console.log('worked!')
-	$scope.keyword_score = function (netloc, keyword) {
-		Validation.get({url: $scope.myurl}, function(data) {
-			$scope.validate = data.valid_url;
+	$scope.request_data = {'url': '', 'keyword': ''}
+	$scope.keyword_score = function () {
+		var netloc = $scope.request_data.url;
+		var keywords = $scope.request_data.keyword;
+		$http.get("/api/kw-score/"+'?url='+ netloc +'&keywords=' + keywords)
+		.then(function(response) {
+			$scope.myWelcome = response.data;
 		});
 	};
 }]);
