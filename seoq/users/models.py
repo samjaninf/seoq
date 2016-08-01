dadaea0696d1e99ca8ff342b42951be26fbb4831
JulianBonilla7 <julian.felipe.bonilla@correounivalle.edu.co>
2016-08-01 16:27:13 -0500

    SEOQ-55 Add fields to update profile

--- seoq/users/models.py
+++ seoq/users/models.py
@@ -14,6 +14,14 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    fb_account = models.URLField(blank=True, max_length=255)
+    twitter_account = models.URLField(blank=True, max_length=255)
+    linkedin_account = models.URLField(blank=True, max_length=255)
+    title = models.CharField(blank=True, max_length=255)
+    about = models.TextField(_("About You"), blank=True, default='')
+    profile_picture = models.ImageField(
+        null=True,
+        upload_to='profile/')
 
     def __str__(self):
         return self.username
