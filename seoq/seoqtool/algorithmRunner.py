7dbafa61bee1c5fb75787ce1d35ef42e4b5884dd
Intern <intern@Blakes-Air.lan>
2016-08-01 11:41:37 -0400

    updates to algorithm runner

--- seoq/seoqtool/algorithmRunner.py
+++ seoq/seoqtool/algorithmRunner.py
@@ -4,12 +4,14 @@ from .algorithm import Algorithm
 class AlgorithmRunner(object):
 
     def run(self):
-        alg = Algorithm()
+        counter = 0
         for backlinkvar in range(3):
             for trustflowvar in range(3):
                 for robotsvar in range(3):
                     for listingvar in range(3):
                         for keyVar in range(3):
+                            print counter
+                            alg = Algorithm()
                             alg.changeVar(
                                 backlinkvar * 2,
                                 trustflowvar, robotsvar * 3,
@@ -19,15 +21,17 @@ class AlgorithmRunner(object):
                                         backlinkvar,
                                         trustflowvar, robotsvar,
                                         listingvar, keyVar)
+                                    print backlinkvar + " " + trustflowvar + " " + robotsvar + " " + listingvar + " " + keyVar
+                            counter = counter + 1
         return "no optimal weights"
 
     def runSites(self, algorithm):
-        print 'reached'
         sites = ['en.wikipedia.org/wiki/Agile_software_development',
                  # 'howdesign.com/editors-picks/dont-go-chasing-waterfalls-agile-web-design/'
                  # 'sixrevisions.com/web-development/agile/',
                  'amazon.com/Agile-Development-Rails-Pragmatic-Programmers/dp/1934356549',
                  'agileana.com/']
+        print sites
         scores = [100.00, 100.00, 100.00, 100.00, 100.00]
         for count in range(len(sites)):
             scores[count] = algorithm.getKeywordScore(
@@ -35,5 +39,4 @@ class AlgorithmRunner(object):
             if count > 0:
                 if scores[count] > scores[count - 1]:
                     return False
-            print scores[count]
         return True
