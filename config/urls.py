b573229e904014d30c11a1d3ff1169237acfc629
brayan15 <bralavar@gmail.com>
2016-08-01 11:20:13 -0500

    SEOQ-58 Log in / Sign up page frontend

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
         name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'),
         name='about'),
+    url(r'^join/$', TemplateView.as_view(template_name='pages/join.html'),
+        name='join'),
 
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, include(admin.site.urls)),
