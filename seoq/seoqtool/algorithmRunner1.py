66d29913487a3c25fc4058c473e16a8afb6d1933
Intern <intern@Blakes-Air.lan>
2016-08-02 14:42:03 -0400

    updates to algorithm with weightings

--- seoq/seoqtool/algorithmRunner1.py
+++ seoq/seoqtool/algorithmRunner1.py
@@ -12,28 +12,28 @@ class AlgorithmRunner1(object):
         print 'done 2'
         three = Algorithm('expedia.com/Washington-Hotels.d178318.Travel-Guide-Hotels')
         print 'done 3'
-        four = Algorithm('https://www.hipmunk.com/Hotels-in-Washington-DC')
+        four = Algorithm('hipmunk.com/Hotels-in-Washington-DC')
         print 'done 4'
-        five = Algorithm('http://www.marriott.com/hotel-search/washington-dc.hotels.united-states/')
+        five = Algorithm('marriott.com/hotel-search/washington-dc.hotels.united-states/')
         print 'done 5'
 
         one.getKeywordClass('hotels.com/de1467359/hotels-washington-district-of-columbia/', self.key)
         print 'done 1'
-        two.getKeywordClass('https://www.tripadvisor.com/Hotels-g28970-Washington_DC_District_of_Columbia-Hotels.html', self.key)
+        two.getKeywordClass('tripadvisor.com/Hotels-g28970-Washington_DC_District_of_Columbia-Hotels.html', self.key)
         print 'done 2'
-        three.getKeywordClass('https://www.expedia.com/Washington-Hotels.d178318.Travel-Guide-Hotels', self.key)
+        three.getKeywordClass('expedia.com/Washington-Hotels.d178318.Travel-Guide-Hotels', self.key)
         print 'done 3'
-        four.getKeywordClass('https://www.hipmunk.com/Hotels-in-Washington-DC', self.key)
+        four.getKeywordClass('hipmunk.com/Hotels-in-Washington-DC', self.key)
         print 'done 4'
-        five.getKeywordClass('http://www.marriott.com/hotel-search/washington-dc.hotels.united-states/', self.key)
+        five.getKeywordClass('marriott.com/hotel-search/washington-dc.hotels.united-states/', self.key)
         print 'done 5'
 
         algs = [one, two, three, four, five]
-        for backlinkvar in range(3):
+        for backlinkvar in range(4):
             for trustflowvar in range(3):
                 for robotsvar in range(3):
                     for listingvar in range(3):
-                        for keyVar in range(3):
+                        for keyVar in range(4):
                             print 'change'
                             for alg in algs:
                                 alg.changeVar(
@@ -41,12 +41,7 @@ class AlgorithmRunner1(object):
                                     trustflowvar + 1, robotsvar * 3 + 1,
                                     listingvar * 3 + 1, keyVar * 3 + 1)
                             if self.runSites(algs):
-                                print '__________Solution___________'
-                                print (backlinkvar + 1) * 2
-                                print (trustflowvar + 1)
-                                print (robotsvar + 1) * 3
-                                print (listingvar + 1) * 3
-                                print (keyVar + 1) * 3
+                                print str(backlinkvar) + '' + str(trustflowvar) + '' + str(robotsvar) + '' + str(listingvar) + '' + str(keyVar)
         return "done"
 
     def runSites(self, algs):
@@ -54,7 +49,7 @@ class AlgorithmRunner1(object):
         for count in range(5):
             alg = algs[count]
             scores[count] = alg.getKeywordScore(self.key)
-            if count > 0:
+            if count > 0 and count < 4:
                 if scores[count] > scores[count - 1]:
                     return False
         return True
