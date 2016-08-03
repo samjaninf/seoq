2de32b61e265798d89332b1277337962980770b3
Cristian Vargas <cristian@swapps.co>
2016-08-03 13:51:08 -0500

    Fix syntax error

--- config/settings/common.py
+++ config/settings/common.py
@@ -303,7 +303,7 @@ PLANS_VALIDATORS = {
 FACEBOOK_ACCESS_TOKEN = env('FACEBOOK_ACCESS_TOKEN')
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-    }
+    )
 }
