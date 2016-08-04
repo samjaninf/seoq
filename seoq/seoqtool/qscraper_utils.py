b3cb013626ced7676d3356e66b0534b6e91c5f28
pranav-gulati <pgulati18@gmail.com>
2016-08-04 14:58:58 -0400

    Add facebook and instagram

--- seoq/seoqtool/qscraper_utils.py
+++ seoq/seoqtool/qscraper_utils.py
@@ -188,15 +188,16 @@ class QscraperSEOQTool(object):
 
     def get_facebook_likes(self):
         access_token = settings.FACEBOOK_ACCESS_TOKEN
-        page_id = "idiotinside"
-        api_endpoint = "https://graph.facebook.com/v2.5/"
-        fb_graph_url = api_endpoint+page_id+"?fields=id,likes,link&access_token="+access_token
+        socialDict = self.JSONObject['social_data']
+        face = socialDict['facebook'][0].split("/")
+        page_id = str(face[-1])
+        api_endpoint = "https://graph.facebook.com/v2.4/"
+        fb_graph_url = api_endpoint+page_id+"?fields=likes&access_token="+access_token
         try:
            api_request = urllib2.Request(fb_graph_url)
            api_response = urllib2.urlopen(api_request)
-           
            try:
-               return json.loads(api_response.read())
+               return json.loads(api_response.read())['likes']
            except (ValueError, KeyError, TypeError):
                return "JSON error"
 
@@ -213,3 +214,20 @@ class QscraperSEOQTool(object):
         auth = tweepy.OAuthHandler('auqkcMIRZXXkZzJT8pfilYTst', '7WB7moJ5KGaFiLb2rffUcE7hardWA0hPJomuLxW8C4QaUJxZB6')
         api = tweepy.API(auth)
         return api.get_user(phrase).followers_count
+
+    def get_instagram_followers(self):
+        api_endpoint = "https://api.instagram.com/v1/tags/nofilter/media/recent?access_token="
+        fb_graph_url = api_endpoint+access_token
+        try:
+           api_request = urllib2.Request(fb_graph_url)
+           api_response = urllib2.urlopen(api_request)
+           try:
+               return json.loads(api_response.read())['followers']
+           except (ValueError, KeyError, TypeError):
+               return "JSON error"
+
+        except IOError, e:
+           if hasattr(e, 'code'):
+               return e.code
+           elif hasattr(e, 'reason'):
+               return e.reason
