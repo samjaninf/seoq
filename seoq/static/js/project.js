f48bd8c3ac8c5926073322b1b93ffbf290feffac
JulianBonilla7 <julian.felipe.bonilla@correounivalle.edu.co>
2016-08-31 00:25:14 -0500

    SEOQ-219 Frontend for Q&A Main screen and responsive

--- seoq/static/js/project.js
+++ seoq/static/js/project.js
@@ -1,4 +1,8 @@
 /* Project specific Javascript goes here. */
+$('#question-tabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
 function showLength(id){
   document.getElementById(id+'-span').innerHTML = document.getElementById(id).value.length;
 }
