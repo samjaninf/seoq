cc1029ed9a96d17bded50d7db16989af7f7b5f5a
brayan <bralavar@gmail.com>
2016-09-19 08:21:51 -0500

    SEOQ-240 Re-order edit profile fields

--- seoq/users/forms.py
+++ seoq/users/forms.py
@@ -7,10 +7,19 @@ class EditProfileForm(forms.ModelForm):
     #profile_picture = forms.ImageField(
     #    required=False)
 
-    areas_of_expertise = ArraySelect(forms.CharField(max_length=50), widget=ArrayWidget(choices=User.EXPERTISE_CHOICES))
-    languages = ArraySelect(forms.CharField(max_length=50), widget=ArrayWidget(choices=User.LANGUAGE_CHOICES))
+    areas_of_expertise = ArraySelect(forms.CharField(max_length=50), widget=ArrayWidget(choices=User.EXPERTISE_CHOICES),required=False)
+    areas_of_expertise_other = forms.CharField(max_length=50, required=False, label='Other areas of expertise')
+    languages = ArraySelect(forms.CharField(max_length=50), widget=ArrayWidget(choices=User.LANGUAGE_CHOICES),required=False)
     title = forms.CharField( required=False, 
       widget=forms.TextInput(attrs={'placeholder': 'e.g. SEO Professional and Marketing Consultant'}))
+    about = forms.CharField(required=False,label='Your Bio', widget=forms.Textarea)
+    location = forms.CharField(required=False,label='Current Location')
+    languages_other = forms.CharField(required=False,label='Other Languages',max_length=50)
+    fb_account = forms.URLField(required=False, max_length=255,label='Facebook Page')
+    twitter_account = forms.URLField(required=False, max_length=255,label='Twitter Page')
+    linkedin_account = forms.URLField(required=False, max_length=255,label='Linkedin Page')
+    google_account = forms.URLField(required=False, max_length=255,label='Google+ Page')
+    skype = forms.CharField(max_length=50, required=False, label='Skype username')
 
     class Meta:
         model = User
